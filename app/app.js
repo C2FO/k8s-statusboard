ba9a774c9056e657ab46429e0c1992f96f3ee36b
Jonathan Stacks <jonstacks13@gmail.com>
2017-11-05 15:06:20 -0600

    Use angular directive for easier templating

--- app/app.js
+++ app/app.js
@@ -1,42 +1,65 @@
-
-// var app = angular.module('k8sStatusApp');
-// app.controller('mainController', function($scope) {
-//     // create a message to display in our view
-//     $scope.title = 'K8S StatusBoard';
-// });
-
-// Initially get the contexts and add divs for them to the dom.
-$.get("/api/contexts", function(data, status){
-  data.forEach(function(context){
-    $("#context-container").append('<div id="context-' + context + '" ></div>');
-  });
-});
-
-// getRuningFailed returns the number of running pods and the number of failed
-// pods
-function getPodMetrics(pods) {
-  var metrics = {
-    running: 0,
-    succeeded: 0,
-    notRunning: 0,
-  };
-  pods.forEach(function(pod){
-    if (pod.status.phase == "Succeeded"){
-      metrics.succeeded += 1;
-    } else if (pod.status.phase == "Running") {
-      metrics.running += 1;
-    } else {
-      metrics.notRunning += 1;
+function Metrics(pods) {
+  this.running = 0;
+  this.notRunning = 0;
+  this.succeeded = 0;
+  if (arguments.length > 0) {
+    for (var i=0; i < pods.length; i ++){
+      var phase = pods[i].status.phase;
+      if (phase == "Succeeded"){
+        this.succeeded += 1;
+      } else if (phase == "Running"){
+        this.running += 1;
+      } else {
+        this.notRunning += 1;
+      }
     }
-  });
-  return metrics;
+  }
 }
 
-// Event source to 
-var es = new EventSource("/events/");
-es.addEventListener("pod-status", function(e){
-  var obj = JSON.parse(e.data);
-  var container = $("div#context-" + obj.context).empty();
-  var metrics = getPodMetrics(obj.pods);
-  container.first().html('<div class="col s4"><h4>' + obj.context + "</h4>" + "<p>Running: " + metrics.running + " Not Running: " + metrics.notRunning + "</p></div>");
+function Context(name) {
+  var self = this;
+  this.name = name;
+  this.metrics = new Metrics();
+  this.update = function(pods) {
+    self.metrics = new Metrics(pods);
+  };
+}
+
+// Begin angular App
+angular.module('k8sStatusApp', [])
+.controller('mainController', function($scope, $http) {
+    // create a message to display in our view
+    $scope.title = 'K8S StatusBoard';
+    $scope.contexts = [];
+
+    // Load the initial contexts
+    $http.get("/api/contexts")
+      .success(function(data){
+        data.forEach(function(contextName){
+          $scope.contexts.push(new Context(contextName));
+        });
+      });
+
+    // Event source to 
+    var es = new EventSource("/events/");
+    es.addEventListener("pod-status", function(e){
+      var obj = JSON.parse(e.data);
+      for(var i = 0; i < $scope.contexts.length; i++){
+        if($scope.contexts[i].name == obj.context) {
+          $scope.contexts[i].update(obj.pods);
+          $scope.$apply();
+        }
+      }
+    });
+})
+.directive('contextMetrics', function(){
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      name: '=',
+      metrics: '='
+    },
+    templateUrl: 'context-metrics.html',
+  };
 });
